7b3374739cf0bae53ee502c5b44e9dc1f34ee99a
rh0155689 <59750306+rh0155689@users.noreply.github.com>
2020-04-28 14:09:33 -0500

    changed veggie cbo to lv

--- Wee13Group-GroupProject/SubwayOrderSystem/src/subwayordersystem/SubwayOrderSystem.java
+++ Wee13Group-GroupProject/SubwayOrderSystem/src/subwayordersystem/SubwayOrderSystem.java
@@ -5,7 +5,13 @@
  */
 package subwayordersystem;
 
+import static java.util.stream.DoubleStream.builder;
+import static java.util.stream.IntStream.builder;
+import static java.util.stream.Stream.builder;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,6 +22,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
@@ -33,6 +40,8 @@ public class SubwayOrderSystem extends Application {
     private String[] meat = {"Without meat", "Ham", "Turkey", "Chicken Breast", "Tuna", "Meatball", "Roast Beef", "Steak"};    
     private String[] cheese = {"Without cheese", "American", "Pepper Jack", "Cheddar", "Provolone", "Swiss"};
     private String[] veggies = {"Without veggies", "Lettuce", "Tomatoe", "Onion", "Pickle", "Black Olive", "Green Peppers", "Jalapenos", "Salad Peppers"};
+            
+
     private String[] sauce = {"Without sauce", "Mustard", "Mayo", "Spicy Mustard", "Honey Mustard", "Southwest sauce", "Ranch sauce", "Sweet-onion sauce"};
     private Boolean saltAndPepper = false;
     
@@ -44,8 +53,11 @@ public class SubwayOrderSystem extends Application {
   private ComboBox<String> cboMeat = new ComboBox<>(); 
   // Create a combo box for selecting cheese
   private ComboBox<String> cboCheese = new ComboBox<>();
-  // Create a combo box for selecting veggies  
-  private ComboBox<String> cboVeggies = new ComboBox<>();
+  // Create a combo box for selecting veggies
+  private ListView<String> lvVeggies = new ListView<>();
+  
+
+    
   // Create a combo box for selecting sauce
   private ComboBox<String> cboSauce = new ComboBox<>();
   /** Text area for displaying text */  
@@ -85,31 +97,33 @@ public class SubwayOrderSystem extends Application {
         cboCheese.setPrefWidth(400);
         cboCheese.setValue(" ");
         
+//        //veggies = new ListView<>();
+        gridPane.add(new Label(" Select type of veggies: "), 0, 4); 
+        gridPane.add(lvVeggies, 1, 4, 1, 1);
+        //veggies.getItems().addAll("Without veggies", "Lettuce", "Tomatoe", "Onion", "Pickle", "Black Olive", "Green Peppers", "Jalapenos", "Salad Peppers");
+        lvVeggies.setPrefWidth(260);
+        lvVeggies.setMaxWidth(260);
+        lvVeggies.setPrefHeight(150);        
+        
+        
+        
         
         gridPane.setHgap(5);
         gridPane.setVgap(5);
-        gridPane.add(new Label(" Select type of veggies: "), 0, 4);       
-        gridPane.add(cboVeggies, 1, 4);
-        cboVeggies.setPrefWidth(400);
-        cboVeggies.setValue(" ");
-        
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
-        gridPane.add(new Label(" Select type of sauce: "), 0, 5);
-        gridPane.add(cboSauce, 1, 5);        
+        gridPane.add(new Label(" Select type of sauce: "), 0, 7);
+        gridPane.add(cboSauce, 1, 7);        
         cboSauce.setPrefWidth(400);
-        cboSauce.setValue(" ");
-        
+        cboSauce.setValue(" ");        
         
         Button btnBuild = new Button(" Build Sandwich");        
         gridPane.setHgap(5);
         gridPane.setVgap(5);
-        gridPane.add(btnBuild, 1, 7);
+        gridPane.add(btnBuild, 0, 9);
         
         CheckBox sAP = new CheckBox("Salt and Pepper");
         gridPane.setHgap(5);
         gridPane.setVgap(5);
-        gridPane.add(sAP, 1, 6); 
+        gridPane.add(sAP, 1, 8); 
         sAP.setSelected(false);
         
         // Create a scroll pane to hold the text area
@@ -117,42 +131,39 @@ public class SubwayOrderSystem extends Application {
         
         gridPane.setHgap(5);
         gridPane.setVgap(10);        
-        gridPane.add(scrollPane, 1, 8);        
+        gridPane.add(scrollPane, 1, 9);        
         scrollPane.setPrefWidth(400);             
         
     ObservableList<String> length = 
       FXCollections.observableArrayList(size);
-    cboSize.getItems().addAll(length); // Add items to combo box
-    //pane.setCenter(descriptionPane);
+    cboSize.getItems().addAll(length); // Add items to combo box    
     
     ObservableList<String> breadItem = 
       FXCollections.observableArrayList(bread);
-    cboBread.getItems().addAll(breadItem); // Add items to combo box
-    //pane.setCenter(descriptionPane);
+    cboBread.getItems().addAll(breadItem); // Add items to combo box    
     
     ObservableList<String> meatItem = 
       FXCollections.observableArrayList(meat);
-    cboMeat.getItems().addAll(meatItem); // Add items to combo box
-    //pane.setCenter(descriptionPane);
+    cboMeat.getItems().addAll(meatItem); // Add items to combo box    
     
     ObservableList<String> cheeseItem = 
       FXCollections.observableArrayList(cheese);
     cboCheese.getItems().addAll(cheeseItem); // Add items to combo box
-    //pane.setCenter(descriptionPane);    
     
-    ObservableList<String> veggieItem = 
+    ObservableList<String> veggieItems = 
       FXCollections.observableArrayList(veggies);
-    cboVeggies.getItems().addAll(veggieItem); // Add items to combo box
-    //pane.setCenter(descriptionPane);
+    lvVeggies.setItems(veggieItems);
+    lvVeggies.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);   
     
     ObservableList<String> sauceItem = 
       FXCollections.observableArrayList(sauce);
-    cboSauce.getItems().addAll(sauceItem); // Add items to combo box
-    //pane.setCenter(descriptionPane);
+    cboSauce.getItems().addAll(sauceItem); // Add items to combo box    
     
     taDescription.setFont(new Font("Serif", 14));    
     taDescription.setWrapText(true);
     taDescription.setEditable(false); 
+    
+    //Label label = new Label();
     
     btnBuild.setOnAction(new EventHandler<ActionEvent>() {
         @Override // Override the handle method
@@ -160,21 +171,31 @@ public class SubwayOrderSystem extends Application {
             String s = (cboSize.getValue());
             String b = (cboBread.getValue());
             String m = (cboMeat.getValue());
-            String c = (cboCheese.getValue());
-            String v = (cboVeggies.getValue());
+            String c = (cboCheese.getValue());  
+                   
             String sa = (cboSauce.getValue());
             
+            lvVeggies.getSelectionModel().selectedItemProperty()
+            .addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
+             ObservableList<String> selectedItems = lvVeggies.getSelectionModel().getSelectedItems();
+
+             StringBuilder builder = new StringBuilder(" ");               
+
+             for (String name : selectedItems) {
+              builder.append(name + "\n"); 
+             }                 
+
             if(sAP.isSelected()){
                 saltAndPepper = true;
             taDescription.appendText("You would like a: \n" + s + " sub sandwich,\n on " + b + " bread,\n With: \n" + m + ",\n" + c +
-                     " cheese,\n" + v + "\nand\n" + sa + "\nAdd salt and pepper"); 
+                     ",\n" + builder.toString() +  " and\n" + sa + "\nAdd salt and pepper"); 
             }else {
             taDescription.appendText("You would like a: \n" + s + " sub sandwich,\n on " + b + " bread,\n With: \n" + m + ",\n" + c + 
-                    " cheese,\n" + v + "\nand\n" + sa); 
+                    ",\n" + builder.toString() + " and\n" + sa + "\nWithout salt and pepper"); 
             }
-            
+           }); 
         }
-        });
+    });
         
         Scene scene = new Scene(gridPane, 550, 500);
         primaryStage.setTitle("Subway Order System");
